4facb2652639644760cc6dd44b04a9c5c0c52661
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-12-22 08:52:46 -0800

    Final hsh file

--- getLine.c
+++ getLine.c
@@ -43,6 +43,7 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 	}
 	return (r);
 }
+
 /**
  * get_input - gets a line minus the newline
  * @info: parameter struct
@@ -85,6 +86,7 @@ ssize_t get_input(info_t *info)
 	*buf_p = buf; /* else not a chain, pass back buffer from _getline() */
 	return (r); /* return length of buffer from _getline() */
 }
+
 /**
  * read_buf - reads a buffer
  * @info: parameter struct
@@ -104,11 +106,13 @@ ssize_t read_buf(info_t *info, char *buf, size_t *i)
 		*i = r;
 	return (r);
 }
+
 /**
  * _getline - gets the next line of input from STDIN
  * @info: parameter struct
  * @ptr: address of pointer to buffer, preallocated or NULL
  * @length: size of preallocated ptr buffer if not NULL
+ *
  * Return: s
  */
 int _getline(info_t *info, char **ptr, size_t *length)
@@ -128,15 +132,18 @@ int _getline(info_t *info, char **ptr, size_t *length)
 	r = read_buf(info, buf, &len);
 	if (r == -1 || (r == 0 && len == 0))
 		return (-1);
+
 	c = _strchr(buf + i, '\n');
 	k = c ? 1 + (unsigned int)(c - buf) : len;
 	new_p = _realloc(p, s, s ? s + k : k + 1);
 	if (!new_p) /* MALLOC FAILURE! */
 		return (p ? free(p), -1 : -1);
+
 	if (s)
 		_strncat(new_p, buf + i, k - i);
 	else
 		_strncpy(new_p, buf + i, k - i + 1);
+
 	s += k - i;
 	i = k;
 	p = new_p;
@@ -145,4 +152,16 @@ int _getline(info_t *info, char **ptr, size_t *length)
 		*length = s;
 	*ptr = p;
 	return (s);
+}
+/**
+ * sigintHandler - blocks ctrl-C
+ * @sig_num: the signal number
+ *
+ * Return: void
+ */
+void sigintHandler(__attribute__((unused))int sig_num)
+{
+	_puts("\n");
+	_puts("$ ");
+	_putchar(BUF_FLUSH);
 }
