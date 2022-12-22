2edd5016df2a6cd4d9c599dea440cba4769c0ff9
Khali3sa <yvonnekhaliesa@gmail.com>
2022-12-22 06:09:47 -0800

    shell commit

--- getLine.c
+++ getLine.c
@@ -109,7 +109,6 @@ ssize_t read_buf(info_t *info, char *buf, size_t *i)
  * @info: parameter struct
  * @ptr: address of pointer to buffer, preallocated or NULL
  * @length: size of preallocated ptr buffer if not NULL
- * 
  * Return: s
  */
 int _getline(info_t *info, char **ptr, size_t *length)
@@ -129,7 +128,6 @@ int _getline(info_t *info, char **ptr, size_t *length)
 	r = read_buf(info, buf, &len);
 	if (r == -1 || (r == 0 && len == 0))
 		return (-1);
-
 	c = _strchr(buf + i, '\n');
 	k = c ? 1 + (unsigned int)(c - buf) : len;
 	new_p = _realloc(p, s, s ? s + k : k + 1);
@@ -139,7 +137,6 @@ int _getline(info_t *info, char **ptr, size_t *length)
 		_strncat(new_p, buf + i, k - i);
 	else
 		_strncpy(new_p, buf + i, k - i + 1);
-	
 	s += k - i;
 	i = k;
 	p = new_p;
